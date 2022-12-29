e58be2502af6d2b11a0ac013d48f0b33d5acbb2f
acielle <sg23mj@gmail.com>
2022-12-29 21:05:16 +0100

    fix: linter warning

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -23,9 +23,7 @@
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  Rectangle.prototype.getArea = function () {
-    return this.width * this.height;
-  };
+  Rectangle.prototype.getArea = () => this.width * this.height;
 }
 
 
